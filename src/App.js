2c94ec6fed40cc7607b884c9b542e61dd5bd888d
Forejy <joffrey.marques@gmail.com>
2021-08-30 16:39:38 +0200

    Exercice terminé

--- src/App.js
+++ src/App.js
@@ -1,16 +1,113 @@
 import './App.css';
 import React from 'react';
-
-class Task extends React.Component {
-
-}
+import Overview from './components/Overview.js'
 
 class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      task: { text: '', edit: false },
+      tasksBuffer: [],
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {text: e.target.value, edit: false },
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.task.text !== '') {
+      this.setState({
+        tasks: this.state.tasks.concat(this.state.task),
+        task: {text: '', edit: false },
+        tasksBuffer: this.state.tasksBuffer.concat(this.state.task.text),
+      })
+    }
+  }
+
+  handleClickDelete(i) { //TODO: Je delete la task, qu'est-ce qu'il reste dans le Buffer ?
+    const temp = this.state.tasks.slice();
+    temp.splice(i, 1);
+
+    this.setState({
+      tasks: temp,
+    })
+  }
+
+  handleClickEdit(i) {
+    let tasks = this.state.tasks.slice();
+    tasks[i] = { text: tasks[i].text, edit: true }; //TODO: réécrire -> task[i].edit = true ?
+
+    this.setState({
+      tasks: tasks,
+    })
+  }
+
+  handleChangeEdit = (i, e) => {
+    const task = e.target.value;
+    const tasksBuffer = this.state.tasksBuffer.slice();
+    tasksBuffer[i] = task;
+
+    this.setState({
+      tasksBuffer: tasksBuffer,
+    })
+  }
+
+  handleSubmitEdit = (i, e) => {
+    e.preventDefault();
+
+    const tasks = this.state.tasks.slice();
+    const tasksBuffer = this.state.tasksBuffer.slice();
+    tasks[i] = { text: tasksBuffer[i], edit: false } ;
+
+    this.setState({
+      tasks: tasks,
+    })
+  }
+
+  handleCancelEdit = (i) => {
+    const tasksBuffer = this.state.tasksBuffer.slice();
+    const tasks = this.state.tasks.slice();
+    tasksBuffer[i] = tasks[i].text
+    tasks[i].edit = false;
+
+    this.setState ({
+      tasksBuffer: tasksBuffer,
+    })
+  }
+
   render() {
+    const { task } = this.state
+
     return (
-      <ul>
-        <div>Test !</div>
-      </ul>
+      <div>
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor="taskInput">Ajouter une tache</label>
+          <input
+            type="text"
+            id="taskInput"
+            value={ task.text }
+            onChange={ this.handleChange }
+          />
+          <button type="submit">
+            Envoyer
+          </button>
+        </form>
+        <Overview
+            tasks={ this.state.tasks }
+            tasksBuffer= { this.state.tasksBuffer }
+            onClickDelete={ (i) => this.handleClickDelete(i) }
+            onClickEdit= { (i) => this.handleClickEdit(i) }
+            onChange={ this.handleChangeEdit }
+            onSubmit={ this.handleSubmitEdit }
+            onCancel={ (i) => this.handleCancelEdit(i) }
+          />
+      </div>
     )
   }
 }
---
+++ src/components/Overview.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  renderTask(task, i) {
+    let deletePart = <span onClick={ () => this.props.onClickDelete(i) }>  X</span>
+    let ret;
+    if (task.edit === true) {
+      ret =
+        <li key={i} id={i} > {i}
+          <form onSubmit={ (e) => this.props.onSubmit(i, e) }>
+            <input
+              type="text"
+              value={ this.props.tasksBuffer[i] }
+              onChange={ (e) => this.props.onChange(i, e) }
+            />
+            <button type="submit">
+              Envoyer
+            </button>
+            <button type="button" onClick= { () => this.props.onCancel(i) }>
+              Annuler
+            </button>
+          </form>
+        </li>
+    }
+    else {
+      ret = <li key={i} id={i} > {i} {task.text} {deletePart} <span onClick={ () => this.props.onClickEdit(i) }> Edit</span></li>
+    }
+  return(ret);
+  }
+
+  render () {
+    const { tasks } = this.props
+    let taskrender;
+
+    if (tasks && tasks.length > 0) {
+      taskrender = tasks.map(
+        (task, i) => this.renderTask(task, i)
+      )
+    }
+    return (
+      <ul>
+      { taskrender }
+      </ul>
+    )
+  }
+}
+
+export default Overview;
